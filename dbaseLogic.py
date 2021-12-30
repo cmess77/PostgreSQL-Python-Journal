4d44839758c7f8f96cb7ae7989140ed52e7cca59
Cameron Messer <cmess77@protonmail.com>
2021-12-30 16:53:37 -0500

    Make journal.py more clear
    Add promptNewEntry to journal.py (prompts user, stores content and date, appends entry to entries list).
    Add viewEntries to journal.py (takes list as arg., prints all entries in list).
    Add getEntries to dbaseLogic.py (takes no args., returns list).
    On branch working
    Changes to be committed:
    	modified:   dbaseLogic.py
    	modified:   journal.py

--- dbaseLogic.py
+++ dbaseLogic.py
@@ -1,13 +1,9 @@
 entries = []
 
 
-def addEntry():
-    entry_content = input("What have you learned today? ")
-    entry_date = input("Enter the date: ")
-
+def addEntry(entry_content, entry_date):
     entries.append({"content": entry_content, "date": entry_date})
 
 
-def viewEntries():
-    for entry in entries:
-        print(f"{entry['date']}\n{entry['content']}\n\n")
+def getEntries():
+    return entries
